76fa7aac2c20a5a683e7d61c652af660b7065c34
hwanghyeonseo <hhseo1202@naver.com>
2026-03-18 13:27:33 +0900

    fix

--- 0317_1_Instrumented_queueing_system_simulation_model.c
+++ 0317_1_Instrumented_queueing_system_simulation_model.c
@@ -68,52 +68,52 @@ int geometric(real p)
 void simulate_stats(double Pa, double Ps, int Te)
 {
     int n = 0;
-    int time = 0;
-    int t1 = 0;
+    int time = 0; //현재 시간
+    int t1 = 0; //첫 고객은 즉시 도착
     int t2 = Te + 1;
 
-    long long C = 0;          
-    double area_n = 0.0;       
-    double busy = 0.0;         
-    int tn = 0;                
+    long long C = 0;    //지금까지 몇 명 완료되었는가 
+    double area_n = 0.0;    //시간 평균 고객수 몇이었는가   
+    double busy = 0.0;    //서버가 얼마나 바빴는가
+    int tn = 0;    //직전 이벤트 시각           
 
     while (time < Te) {
         if (t1 < t2) {
-            time = t1;
-            if (time > Te) time = Te;
+            time = t1; //도착 발생
+            if (time > Te) time = Te; //다음 이벤트가 종료 시각 이후라면, 그냥 Te까지만 반영하기 위한 안전장치
 
-            area_n += (double)n * (time - tn);
-            if (n > 0) busy += (double)(time - tn);
+            area_n += (double)n * (time - tn); //고객 수 면적 누적
+            if (n > 0) busy += (double)(time - tn); //바쁜시간 누적
             tn = time;
 
-            if (time >= Te) break;
+            if (time >= Te) break; //종료조건
 
             n++;
 
-            t1 = time + geometric(Pa);
+            t1 = time + geometric(Pa); //다음 도착시간 예약
 
-            if (n == 1) {
+            if (n == 1) { //서비스가 비어있으면 서비스 시작
                 t2 = time + geometric(Ps);
             }
         }
-        else {
+        else { //서비스 완료 처리
             time = t2;
-            if (time > Te) time = Te;
+            if (time > Te) time = Te; //서비스 완료가 종료 시각 이후라면, 그냥 Te까지만 반영하기 위한 안전장치
 
             area_n += (double)n * (time - tn);
             if (n > 0) busy += (double)(time - tn);
             tn = time;
 
-            if (time >= Te) break;
+            if (time >= Te) break; //종료조건
 
-            n--;
-            C++;
+            n--; //시스템 내 고객 수 감소
+            C++; //완료 고객 수 증가
 
-            if (n > 0) {
-                t2 = time + geometric(Ps);
+            if (n > 0) { //서비스를 마친 뒤에도 고객이 남아있으면 대기중인 다음 고객이 즉시 서비스에 들어감
+                t2 = time + geometric(Ps); //새 서비스 완료시간 예약
             }
-            else {
-                t2 = Te + 1;
+            else { //고객이 아무도 없으면
+                t2 = Te + 1; //서버는 idle해지므로 예정된 서비스 완료가 없어서 Te+1값을 가지게 됨.
             }
         }
     }
--- 0317_2_smpl_queueing_system_simulation_model.c
+++ 0317_2_smpl_queueing_system_simulation_model.c
@@ -9,6 +9,10 @@ typedef double real;
 #define A 16807L
 #define M 2147483647L
 
+#define ARRIVAL    1
+#define REQUEST    2
+#define COMPLETION 3
+
 static long In[16] = {
     0L,
     1973272912L, 747177549L,  20464843L,  640830765L, 1098742207L,
@@ -54,62 +58,99 @@ int geometric(real p)
 
 int main(void)
 {
-    real Pa = 0.1;     // 도착 확률
-    real Ps = 0.1;     // 서비스 확률
-    int Te = 200;      // 총 시뮬레이션 시간
+    real Pa = 0.1;   /* 도착 확률 */
+    real Ps = 0.1;   /* 서비스 확률 */
+    int Te = 200;    /* 총 시뮬레이션 시간 */
 
     int time = 0;
-    int t1 = 0;        // 다음 arrival 시각
-    int t2 = Te + 1;   // 다음 completion 시각 (없으면 매우 큰 값)
 
-    int next_id = 1;           // 다음 고객 번호
-    int current_customer = -1; // 현재 서비스 중인 고객
-    int server_busy = 0;       // 0: idle, 1: busy
+    /* 다음 이벤트 시각 */
+    int t_arrival = 0;         /* 다음 arrival 시각 */
+    int t_completion = Te + 1; /* 다음 completion 시각, 없으면 큰 값 */
 
+    /* 현재 발생할 이벤트 정보 */
+    int event;
+    int customer;
+
+    /* 고객 번호 */
+    int next_id = 1;
+    int current_customer = -1;
+
+    /* 서버 상태 */
+    int server_busy = 0;
+
+    /* 대기 큐 */
     int queue[10000];
     int front = 0, rear = 0;
 
     while (time < Te) {
 
-        if (t1 < t2) {
-            /* event 1: arrival */
-            int cid;
-
-            time = t1;
-            if (time >= Te) break;
-
-            cid = next_id++;
-            printf("at time %d, customer %d arrive\n", time, cid);
-
-            /* 다음 도착 예약 */
-            t1 = time + geometric(Pa);
-
-            /* event 2: request server */
-            if (!server_busy) {
-                server_busy = 1;
-                current_customer = cid;
-                printf("at time %d, customer %d begin\n", time, current_customer);
-                t2 = time + geometric(Ps);
-            } else {
-                queue[rear++] = cid;
-            }
+        /* cause(&event, &customer) 역할을 직접 구현 */
+        if (t_arrival < t_completion) {
+            event = ARRIVAL;
+            time = t_arrival;
+            customer = next_id;
+        } else {
+            event = COMPLETION;
+            time = t_completion;
+            customer = current_customer;
         }
-        else {
-            /* event 3: completion */
-            time = t2;
-            if (time >= Te) break;
 
-            printf("at time %d, customer %d end\n", time, current_customer);
+        if (time >= Te) break;
+
+        switch (event) {
+
+        case ARRIVAL:
+            /* 고객 도착 */
+            printf("at time %d, customer %d arrive\n", time, customer);
+
+            /* 교수님 코드의
+               schedule(2, 0.0, customer)
+               를 바로 REQUEST case 실행으로 표현 */
+            event = REQUEST;
+
+            /* 다음 고객 도착 예약 */
+            next_id++;
+            t_arrival = time + geometric(Pa);
+
+            switch (event) {
+
+            case REQUEST:
+                /* 서버 요청 */
+                if (!server_busy) {
+                    server_busy = 1;
+                    current_customer = customer;
+                    printf("at time %d, customer %d begin\n", time, customer);
+
+                    /* 서비스 완료 예약 */
+                    t_completion = time + geometric(Ps);
+                } else {
+                    /* 서버가 바쁘면 큐에 대기 */
+                    queue[rear++] = customer;
+                }
+                break;
+            }
+
+            break;
+
+        case COMPLETION:
+            /* 서비스 완료 */
+            printf("at time %d, customer %d end\n", time, customer);
 
             if (front < rear) {
+                /* 대기 중인 다음 고객이 있으면 즉시 시작 */
                 current_customer = queue[front++];
                 printf("at time %d, customer %d begin\n", time, current_customer);
-                t2 = time + geometric(Ps);
+
+                /* 다음 완료 예약 */
+                t_completion = time + geometric(Ps);
             } else {
+                /* 더 이상 대기 고객이 없으면 서버 유휴 */
                 server_busy = 0;
                 current_customer = -1;
-                t2 = Te + 1;
+                t_completion = Te + 1;
             }
+            break;
         }
     }
 
